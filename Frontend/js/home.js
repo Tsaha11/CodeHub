f91537180daf047dc546a34f6e4d87db2ab7e68f
Tsaha11 <tuhin727066@gmail.com>
2023-01-03 21:27:22 +0530

    services and quotes added

---
+++ Frontend/js/home.js
@@ -0,0 +1,16 @@
+const boxes=document.getElementsByClassName('box');
+const quoteText=document.querySelector('.quote-body span');
+const clipboard=document.querySelector('.clipboard');
+const volume=document.querySelector('.volume')
+for(const box of boxes){
+    box.addEventListener('click',(er)=>{
+        window.location.href='/'+box.id;
+    })
+}
+clipboard.addEventListener('click',(er)=>{
+    navigator.clipboard.writeText(quoteText.innerHTML);
+})
+volume.addEventListener('click',(er)=>{
+    let utterance=new SpeechSynthesisUtterance(`${quoteText.innerHTML}`);
+    speechSynthesis.speak(utterance)
+})
